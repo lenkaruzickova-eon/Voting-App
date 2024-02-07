3cc832386b8908ec831430e4181a47c02fde7333
Lenka Růžičková <lenka.ruzickova1@eon.cz>
2024-02-07 18:35:58 +0100

    change of export of formatQuestion function

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -147,7 +147,7 @@ export function _getQuestions() {
   });
 }
 
-export function formatQuestion({ optionOneText, optionTwoText, author }) {
+function formatQuestion({ optionOneText, optionTwoText, author }) {
   return {
     id: generateUID(),
     timestamp: Date.now(),
